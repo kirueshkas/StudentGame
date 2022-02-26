6ac3c3aa1c1844437220eeb5726b03afa18efae6
ildar <Ildar626gtr@gamail.com>
2022-02-26 20:01:36 +0500

    добавил управление яркостью картинки

--- StudentGame/Engine/Season&Time.cs
+++ StudentGame/Engine/Season&Time.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace StudentGame.Engine
@@ -25,6 +26,37 @@ namespace StudentGame.Engine
         public static TimesDay DayTime = DetermineTimeDay();
         private static DateTime timeNow;
 
+        public static Bitmap SetBrightness(Bitmap btmap, int brightness)
+        {
+            Bitmap temp = btmap;
+            Bitmap bmap = (Bitmap)temp.Clone();
+            if (brightness < -255) brightness = -255;
+            if (brightness > 255) brightness = 255;
+            Color c;
+            for (int i = 0; i < bmap.Width; i++)
+            {
+                for (int j = 0; j < bmap.Height; j++)
+                {
+                    c = bmap.GetPixel(i, j);
+                    int cR = c.R + brightness;
+                    int cG = c.G + brightness;
+                    int cB = c.B + brightness;
+ 
+                    if (cR < 0) cR = 1;
+                    if (cR > 255) cR = 255;
+ 
+                    if (cG < 0) cG = 1;
+                    if (cG > 255) cG = 255;
+ 
+                    if (cB < 0) cB = 1;
+                    if (cB > 255) cB = 255;
+ 
+                    bmap.SetPixel(i, j, Color.FromArgb((byte)cR, (byte)cG, (byte)cB));
+                }
+            }
+            return (Bitmap)bmap.Clone();
+        }
+        
         private static Seasons DetermineSeason()
         {
             timeNow = DateTime.Now;
